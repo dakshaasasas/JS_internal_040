a5758cc9554d829d4daa8e4b92c4aae599c27e69
Daksh Singh Pokharia <158048438+dakshaasasas@users.noreply.github.com>
2025-12-13 04:51:53 +0000

    done

--- day3/t3.js
+++ day3/t3.js
@@ -33,3 +33,30 @@ console.log("old City:", person.address.city);
 person.address.city = "Mumbai";
 
 console.log("Updated City:", person.address.city);
+
+
+// Create a normal JavaScript object
+let student = {
+    name: "Rahul",
+    age: 22,
+    course: "JavaScript"
+};
+
+// TASK 15: Convert object to JSON string
+let jsonString = JSON.stringify(student);
+
+// Print JSON string
+console.log("JSON String:");
+console.log(jsonString);
+
+// TASK 16: Convert JSON string back to object
+let objectAgain = JSON.parse(jsonString);
+
+// Print converted object
+console.log("Converted Back Object:");
+console.log(objectAgain);
+
+// Access values after conversion
+console.log("Name:", objectAgain.name);
+console.log("Age:", objectAgain.age);
+console.log("Course:", objectAgain.course);
